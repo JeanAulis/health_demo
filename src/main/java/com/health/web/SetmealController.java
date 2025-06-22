c34c8c876512d2a2ac1d8b1ca422db9ba327f16e
moon <112243222+JeanAulis@users.noreply.github.com>
2025-06-22 22:11:38 +0800

    feat(文件存储): 添加阿里云OSS文件上传功能（未完成）
    - 在pom.xml中添加阿里云OSS SDK及相关依赖
    - 创建AliOssUtil工具类实现文件上传功能
    - 在SetmealController中添加文件上传接口(暂注释)

--- src/main/java/com/health/web/SetmealController.java
+++ src/main/java/com/health/web/SetmealController.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -21,5 +23,14 @@ public class SetmealController {
         log.info("分页查询检查项请求，参数：{}", pageQueryDTO);
         return setmealService.findPage(pageQueryDTO);
     }
+
+//    @PostMapping("/setmeal/upload")
+//    public Result upload(@RequestParam("image") MultipartFile file){
+//        log.info("文件上传：{}", file.getOriginalFilename());
+//        String url = AliyunOssClient.upload(file);
+//        log.info("文件上传完成，结果：{}", url);
+//        return Result.success(url);
+//    }
+
 
 }
